0795bca52467170869c25d9120fd6e5521853ffa
iivko <iiivkoo11@gmail.com>
2025-12-07 21:40:09 +0200

    Render color buffer and free previous allocation

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <SDL.h>
 
 
 bool is_running = false;
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+
+uint32_t *color_buffer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 
 bool initialize_window(void) {
@@ -20,8 +28,8 @@ bool initialize_window(void) {
         NULL,
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        500,
-        500,
+        window_width,
+        window_height,
         SDL_WINDOW_BORDERLESS
     );
 
@@ -43,7 +51,27 @@ bool initialize_window(void) {
 }
 
 void setup(void) {
-    // TODO1:
+    // Create a color buffer for the window.
+    //
+    // Each pixel uses 4 bytes (uint32_t), so the total memory is:
+    //   bytes = 4 * window_width * window_height
+    //
+    // Example for 800x600 window:
+    //   bytes = 4 * 800 * 600 = 1,920,000 bytes
+    //   kilobytes = 1,920,000 / 1024 ≈ 1,875 KB
+    //   megabytes = 1,875 / 1024 ≈ 1.83 MB
+    // Allocate the required memory in bytes to hold the color buffer
+    color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
+
+    // Creating an SDL texture that is used to display the color buffer
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
+
 }
 
 void process_input(void) {
@@ -68,16 +96,53 @@ void process_input(void) {
 }
 
 void update(void) {
-    // TODO3:
+    // TODO:
+}
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        (int)(window_width * sizeof(uint32_t))
+    );
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL
+    );
+}
+
+// color - hex number representing our color
+void clear_color_buffer(uint32_t color) {
+    for (int y = 0; y < window_height; y++) {
+        for (int x = 0; x < window_width; x++) {
+            // Calculate the index of the pixel at column x, row y in the color buffer.
+            // The formula is: index = (window_width * row) + column
+            color_buffer[(window_width * y) + x] = color;
+        }
+    }
+
 }
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
+
+    render_color_buffer();
+    clear_color_buffer(0xFFFFFF00);
 
     SDL_RenderPresent(renderer);
 }
 
+void destroy_window(void) {
+    free(color_buffer);
+
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
 
 int main(int argc, char *argv[]) {
     /* Create SDL window */
@@ -92,6 +157,7 @@ int main(int argc, char *argv[]) {
         render();
     }
 
+    destroy_window();
 
     return 0;
 }
