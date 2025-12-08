0b3b585079e6bdded057f112c2ead7b0510325f4
iivko <iiivkoo11@gmail.com>
2025-12-08 15:37:48 +0200

    split the logic in separate file

---
+++ display.h
@@ -0,0 +1,30 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <SDL.h>
+
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+
+extern uint32_t *color_buffer;
+extern SDL_Texture *color_buffer_texture;
+
+extern int window_width;
+extern int window_height;
+
+
+bool initialize_window(void);
+
+void draw_grid(void);
+void draw_rectangle(int x_cor, int y_cor, int width, int height, uint32_t color);
+
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+
+void destroy_window(void);
+#endif //DISPLAY_H
--- main.c
+++ main.c
@@ -4,62 +4,10 @@
 
 #include <SDL.h>
 
+#include "display.h"
+
 
 bool is_running = false;
-
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
-
-uint32_t *color_buffer = NULL;
-SDL_Texture *color_buffer_texture = NULL;
-
-int window_width;
-int window_height;
-
-
-bool initialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-        return false;
-    }
-
-    // Use SDL to query what is the fullscreen max. Width and Height
-    SDL_DisplayMode display_mode;
-    if (SDL_GetCurrentDisplayMode(0, &display_mode) != 0) {
-        fprintf(stderr, "Error getting display mode: %s\n", SDL_GetError());
-        return false;
-    }
-
-    window_width = display_mode.w;
-    window_height = display_mode.h;
-
-    // Create a SDL Window
-    window = SDL_CreateWindow(
-        NULL,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        window_width,
-        window_height,
-        SDL_WINDOW_BORDERLESS
-    );
-
-    if (!window) {
-        fprintf(stderr, "Error creating SDL Window: %s\n", SDL_GetError());
-        return false;
-    }
-
-
-    // Create an SDL renderer
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL Renderer: %s\n", SDL_GetError());
-        return false;
-    }
-
-    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-    return true;
-}
 
 void setup(void) {
     // Create a color buffer for the window.
@@ -110,88 +58,17 @@ void update(void) {
     // TODO:
 }
 
-void draw_grid(void) {
-    int gap = 10;
-
-    int rows = window_height / gap;
-    int cols = window_width / gap;
-    // SDL_Log("Rows: %d", rows);
-    // SDL_Log("Cols: %d", cols);
-
-
-    // for (int y = 0; y < window_height; y++) {
-    //     for (int x = 0; x < window_width; x++) {
-    //         if (x % gap == 0 || y % gap == 0) {
-    //             color_buffer[(window_width * y) + x] = 0x808080FF;
-    //         }
-    //     }
-    // }
-
-    for (int y = 0; y < rows; y++) {
-        for (int x = 0; x < cols; x++) {
-            color_buffer[(window_width * (y * gap) + x * gap)] = 0x808080FF;
-        }
-    }
-}
-
-void draw_rectangle(int x_cor, int y_cor, int width, int height, uint32_t color) {
-    if (x_cor > window_width || y_cor > window_height) {
-        SDL_Log("Out of bounds\n");
-    }
-
-    for (int y = y_cor; y < y_cor + height; y++) {
-        for (int x = x_cor; x < x_cor + width; x++) {
-            color_buffer[(window_width * y) + x] = color;
-        }
-    }
-}
-
-void render_color_buffer(void) {
-    SDL_UpdateTexture(
-        color_buffer_texture,
-        NULL,
-        color_buffer,
-        (int)(window_width * sizeof(uint32_t))
-    );
-    SDL_RenderCopy(
-        renderer,
-        color_buffer_texture,
-        NULL,
-        NULL
-    );
-}
-
-// color - hex number representing our color
-void clear_color_buffer(uint32_t color) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            // Calculate the index of the pixel at column x, row y in the color buffer.
-            // The formula is: index = (window_width * row) + column
-            color_buffer[(window_width * y) + x] = color;
-        }
-    }
-
-}
-
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     draw_grid();
-    draw_rectangle(1900, 200, 300, 150, 0xFFFF00FF);
+    draw_rectangle(100, 200, 300, 150, 0xFFFF00FF);
 
     render_color_buffer();
     clear_color_buffer(0xFF000000);
 
     SDL_RenderPresent(renderer);
-}
-
-void destroy_window(void) {
-    free(color_buffer);
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 }
 
 int main(int argc, char *argv[]) {
